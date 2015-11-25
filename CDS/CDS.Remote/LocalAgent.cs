383dccb2459bf899e6fb1d7272699cf6d22a6733
Threadnaught <patrickhingley@live.co.uk>
2015-11-25 21:41:48 +0000

    added CDS.Remote

---
+++ CDS/CDS.Remote/LocalAgent.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using CDS.Common;
+
+namespace CDS.Remote
+{
+	public abstract class CDSAgent
+	{
+		public CDSMessageHandler CDSHandler;
+		public abstract void OnReceiveCDSMessage (byte Op, string TgtNode, int OpID, byte[] Body);
+	}
+	public class CDSLocalAgent : CDSAgent
+	{
+		List<SentOp> SentOps = new List<SentOp>();
+		//the side which initiated the channel (sends operations to remote agent)
+		//parsing responses, sending commands
+		public int ChannelID;
+		public override void OnReceiveCDSMessage (byte Op, string TgtNode, int OpID, byte[] Body)
+		{
+			SentOp ToRemove = null;
+			//Response to earilier request
+			foreach(SentOp Sent in SentOps)
+			{
+				if (Sent.OpID == OpID) 
+				{
+					Sent.response = (CDSResponses)Op;
+					Sent.Reply = Body;
+					Sent.OnReply (Sent);
+					ToRemove = Sent;
+					break;
+				}
+			}
+			SentOps.Remove (ToRemove);
+		}
+		public SentOp SendRequest(CDSOperations Op, string TgtNode, byte[] Body)
+		{
+			SentOp ret = null;
+			SendRequest (Op, TgtNode, Body, (SentOp s) => { ret = s; });
+			while (ret == null)	System.Threading.Thread.Sleep (10);
+			return ret;
+		}
+		public void SendRequest(CDSOperations Op, string TgtNode, byte[] Body, Reply OnReply)
+		{
+			int OpID = 0;
+			foreach (SentOp o in SentOps)
+				OpID = Math.Max (OpID, o.OpID + 1);
+			SentOp NewSentOp = new SentOp(){ OpID = OpID };
+			NewSentOp.OnReply += OnReply;
+			SentOps.Add (NewSentOp);
+			CDSHandler.SendMessage (ChannelID, (byte)Op, TgtNode, OpID, Body);
+		}
+		public RemoteNode Root
+		{
+			get
+			{
+				return new RemoteNode (this, "");
+			}
+		}
+	}
+}
---
+++ CDS/CDS.Remote/RemoteNode.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using CDS.Common;
+
+namespace CDS.Remote
+{
+	public class RemoteNode
+	{
+		public CDSLocalAgent agent;
+		public RemoteNode parent;
+		public string FullName;
+		public RemoteNode(CDSLocalAgent Agent, string Name)
+		{
+			FullName = Name;
+			agent = Agent;
+		}
+		public byte[] Read()
+		{
+			SentOp o = agent.SendRequest(CDSOperations.read, FullName, new byte[]{});
+			return o.Reply;
+		}
+		public void Write(byte[] Data)
+		{
+			agent.SendRequest (CDSOperations.write, FullName, Data);
+		}
+		public NodeType Type
+		{
+			get
+			{
+				return (NodeType)agent.SendRequest (CDSOperations.getType, FullName, new byte[0]).Reply [0];
+			}
+		}
+		public RemoteNode Parent
+		{
+			get
+			{
+				if (FullName == "")
+					return null;
+				return new RemoteNode(agent, FullName.Substring (0, FullName.Length - (FullName.Split ('.').Last ().Length + 1)));
+			}
+		}
+		public List<RemoteNode> Children
+		{
+			get
+			{
+				byte[] bs = agent.SendRequest (CDSOperations.getChildren, FullName, new byte[]{ }).Reply;
+				List<string> strs = new List<string> ();
+				strs.Add ("");
+				foreach (byte b in bs) 
+				{
+					if (b != 0) 
+					{
+						strs [strs.Count - 1] += (char)b;
+					}
+					else 
+					{
+						strs.Add ("");
+					}
+				}
+				strs.RemoveAt (strs.Count - 1);
+				List<RemoteNode> rs = new List<RemoteNode> ();
+				foreach (string s in strs) 
+				{
+					rs.Add (new RemoteNode (agent, FullName + "." + s));
+				}
+				return rs;
+			}
+		}
+		public RemoteNode CreateChild(string ChildName, NodeType t)
+		{
+			agent.SendRequest (CDSOperations.create, FullName + "." + ChildName, new byte[]{ (byte)t });
+			return new RemoteNode (agent, FullName + "." + ChildName);
+		}
+		public void Delete()
+		{
+			agent.SendRequest (CDSOperations.delete, FullName, new byte[]{ });
+		}
+	}
+}
