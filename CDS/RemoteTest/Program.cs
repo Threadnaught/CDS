14f1f248a58380a5cc020ec3957e0203b5e0736b
Threadnaught <patrickhingley@live.co.uk>
2015-11-25 21:48:04 +0000

    made remote test program

---
+++ CDS/RemoteTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CDS.Common;
+using CDS.Remote;
+
+namespace RemoteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CDSMessageHandler h = new CDSMessageHandler(System.Net.IPAddress.Parse("127.0.0.1"));
+            CDSRemoteAgent a = (CDSRemoteAgent)h.OpenNewChannel();
+            RemoteNode n = a.Root.CreateChild("Test", NodeType.Data);
+            n.Write(new byte[100000]);
+            Console.WriteLine(n.Read().Length);
+            Console.ReadKey();
+        }
+    }
+}
