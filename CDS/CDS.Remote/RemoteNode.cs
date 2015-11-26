ec77d33ba28a5b089400e8f2b8e0f8e5a06e1e75
Threadnaught <patrickhingley@live.co.uk>
2015-11-26 23:22:46 +0000

    finished making RemoteNode inherit from Node

--- CDS/CDS.Remote/RemoteNode.cs
+++ CDS/CDS.Remote/RemoteNode.cs
@@ -6,33 +6,30 @@ using CDS.Common;
 
 namespace CDS.Remote
 {
-	public class RemoteNode
+	public class RemoteNode : Node
 	{
 		public CDSRemoteAgent agent;
-		public RemoteNode parent;
-		public string FullName;
-		public RemoteNode(CDSRemoteAgent Agent, string Name)
-		{
-			FullName = Name;
-			agent = Agent;
-		}
-		public byte[] Read()
-		{
-			SentOp o = agent.SendRequest(CDSOperations.read, FullName, new byte[]{});
-			return o.Reply;
-		}
-		public void Write(byte[] Data)
-		{
-			agent.SendRequest (CDSOperations.write, FullName, Data);
-		}
-		public NodeType Type
-		{
-			get
-			{
-				return (NodeType)agent.SendRequest (CDSOperations.getType, FullName, new byte[0]).Reply [0];
-			}
-		}
-		public RemoteNode Parent
+        public RemoteNode(CDSRemoteAgent Agent, string Name)
+        {
+            FullName = Name;
+            agent = Agent;
+        }
+
+        public string Name 
+        {
+            get 
+            {
+                return FullName.Substring(FullName.Length - (FullName.Split('.').Last().Length + 1));
+            }
+        }
+        public NodeType Type
+        {
+            get
+            {
+                return (NodeType)agent.SendRequest(CDSOperations.getType, FullName, new byte[0]).Reply[0];
+            }
+        }
+        public new RemoteNode parent
 		{
 			get
 			{
@@ -41,41 +38,50 @@ namespace CDS.Remote
 				return new RemoteNode(agent, FullName.Substring (0, FullName.Length - (FullName.Split ('.').Last ().Length + 1)));
 			}
 		}
-		public List<RemoteNode> Children
+        public new List<RemoteNode> Children
+        {
+            get
+            {
+                byte[] bs = agent.SendRequest(CDSOperations.getChildren, FullName, new byte[] { }).Reply;
+                List<string> strs = new List<string>();
+                strs.Add("");
+                foreach (byte b in bs)
+                {
+                    if (b != 0)
+                    {
+                        strs[strs.Count - 1] += (char)b;
+                    }
+                    else
+                    {
+                        strs.Add("");
+                    }
+                }
+                strs.RemoveAt(strs.Count - 1);
+                List<RemoteNode> rs = new List<RemoteNode>();
+                foreach (string s in strs)
+                {
+                    rs.Add(new RemoteNode(agent, FullName + "." + s));
+                }
+                return rs;
+            }
+        }
+        public override void Delete()
+        {
+            agent.SendRequest(CDSOperations.delete, FullName, new byte[] { });
+        }
+		public override byte[] Read()
 		{
-			get
-			{
-				byte[] bs = agent.SendRequest (CDSOperations.getChildren, FullName, new byte[]{ }).Reply;
-				List<string> strs = new List<string> ();
-				strs.Add ("");
-				foreach (byte b in bs) 
-				{
-					if (b != 0) 
-					{
-						strs [strs.Count - 1] += (char)b;
-					}
-					else 
-					{
-						strs.Add ("");
-					}
-				}
-				strs.RemoveAt (strs.Count - 1);
-				List<RemoteNode> rs = new List<RemoteNode> ();
-				foreach (string s in strs) 
-				{
-					rs.Add (new RemoteNode (agent, FullName + "." + s));
-				}
-				return rs;
-			}
+			SentOp o = agent.SendRequest(CDSOperations.read, FullName, new byte[]{});
+			return o.Reply;
 		}
-		public RemoteNode CreateChild(string ChildName, NodeType t)
+		public override void Write(byte[] Data)
 		{
-			agent.SendRequest (CDSOperations.create, FullName + "." + ChildName, new byte[]{ (byte)t });
-			return new RemoteNode (agent, FullName + "." + ChildName);
+			agent.SendRequest (CDSOperations.write, FullName, Data);
 		}
-		public void Delete()
-		{
-			agent.SendRequest (CDSOperations.delete, FullName, new byte[]{ });
-		}
+        public override Node AddChild(NodeType type, string Name)
+        {
+            agent.SendRequest(CDSOperations.create, FullName + "." + Name, new byte[] { (byte)type });
+            return new RemoteNode(agent, FullName + "." + Name);
+        }
 	}
 }
