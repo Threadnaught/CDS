c2b9c97d73a907acfd07a3d5324121341c4f3e11
Threadnaught <patrickhingley@live.co.uk>
2015-11-29 21:21:42 +0000

    added checks to see if connection is closed in RemoteAgent

--- CDS/CDS.Remote/RemoteAgent.cs
+++ CDS/CDS.Remote/RemoteAgent.cs
@@ -31,20 +31,34 @@ namespace CDS.Remote
 		}
 		public SentOp SendRequest(CDSOperations Op, string TgtNode, byte[] Body)
 		{
-			SentOp ret = null;
-			SendRequest (Op, TgtNode, Body, (SentOp s) => { ret = s; });
-			while (ret == null)	System.Threading.Thread.Sleep (10);
-			return ret;
+            if (CDSHandler.Alive)
+            {
+                SentOp ret = null;
+                SendRequest(Op, TgtNode, Body, (SentOp s) => { ret = s; });
+                while (ret == null) System.Threading.Thread.Sleep(10);
+                return ret;
+            }
+            else 
+            {
+                throw new Exception("connection closed");
+            }
 		}
 		public void SendRequest(CDSOperations Op, string TgtNode, byte[] Body, Reply OnReply)
 		{
-			int OpID = 0;
-			foreach (SentOp o in SentOps)
-				OpID = Math.Max (OpID, o.OpID + 1);
-			SentOp NewSentOp = new SentOp(){ OpID = OpID };
-			NewSentOp.OnReply += OnReply;
-			SentOps.Add (NewSentOp);
-			CDSHandler.SendMessage (ChannelID, (byte)Op, TgtNode, OpID, Body);
+            if (CDSHandler.Alive)
+            {
+                int OpID = 0;
+                foreach (SentOp o in SentOps)
+                    OpID = Math.Max(OpID, o.OpID + 1);
+                SentOp NewSentOp = new SentOp() { OpID = OpID };
+                NewSentOp.OnReply += OnReply;
+                SentOps.Add(NewSentOp);
+                CDSHandler.SendMessage(ChannelID, (byte)Op, TgtNode, OpID, Body);
+            }
+            else
+            {
+                throw new Exception("connection closed");
+            }
 		}
 		public RemoteNode Root
 		{
