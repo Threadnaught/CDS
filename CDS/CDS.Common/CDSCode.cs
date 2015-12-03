f92c8887b98c2ce72121a467ffceea8221864109
Threadnaught <patrickhingley@live.co.uk>
2015-12-03 23:33:08 +0000

    no idea why these file wern't included

---
+++ CDS/CDS.Common/CDSCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS.Common
+{
+    class CDSCode
+    {
+        public string Code;
+        public CDSCode() { }
+        public static CDSCode FromRaw(byte[] Raw) 
+        {
+            return new CDSCode() { Code = Encoding.Unicode.GetString(Raw); };
+        }
+        public byte[] ToRaw() 
+        {
+            return Encoding.Unicode.GetBytes(Code);
+        }
+        public CDSData Read() 
+        {
+            CDSData data;
+            
+        }
+    }
+}
