6af7196c79871c847400f3d7e2a23bcacd0619c8
Threadnaught <patrickhingley@live.co.uk>
2015-12-15 15:39:03 +0000

    continuing

--- CDS/CDS.Common/TcpConnectionPool.cs
+++ CDS/CDS.Common/TcpConnectionPool.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.IO;
 
 namespace CDS.Common
 {
@@ -40,11 +41,66 @@ namespace CDS.Common
         public IPAddress target;
         public void ReceiveFromOtherSideRaw(byte[] Message)
         {
+            CDSMessage message = CDSMessage.FromStream(new MemoryStream(Message));
+        }
+    }
+    public struct CDSMessage
+    {
+        public UInt32 ChannelId;
+        public ChannelOperation ChannelOp;
 
-        }
-        public void SendToOtherSideRaw(byte[] Message)
+        public byte CDSOp;
+        public string TargetNode;
+        public UInt32 Seq;
+        public byte[] Message;
+
+        public MemoryStream ToStream()
         {
-            TcpConnectionPool.SendMessageToOtherSide(target, Id, Message);
+            MemoryStream s = new MemoryStream();
+            s.Write(BitConverter.GetBytes(ChannelId), 0, 4);
+            s.Write(BitConverter.GetBytes((byte)ChannelOp), 0, 1);
+            s.Write(BitConverter.GetBytes(CDSOp), 0, 1);
+            foreach (byte b in Encoding.ASCII.GetBytes(TargetNode)) s.WriteByte(b);
+            s.WriteByte(0);
+            s.Write(BitConverter.GetBytes(Seq), 0, 4);
+            s.Write(Message, 0, Message.Length);
+            return s;
         }
+        public static CDSMessage FromStream(MemoryStream s)
+        {
+            CDSMessage m = new CDSMessage();
+            m.LoadStream(s);
+            return m;
+        }
+        void LoadStream(MemoryStream s)
+        {
+            s.Seek(0, SeekOrigin.Begin);
+            ChannelId = BitConverter.ToUInt32(s.ReadBytes(4), 0);
+            ChannelOp = (ChannelOperation)s.ReadByte();
+            CDSOp = (byte)s.ReadByte();
+            TargetNode = "";
+            int Last = s.ReadByte();
+            while (Last != 0)
+            {
+                TargetNode += (char)Last;
+                Last = s.ReadByte();
+            }
+            Seq = BitConverter.ToUInt32(s.ReadBytes(4), 0);
+            Message = s.ReadBytes((int)(s.Length - s.Position));
+        }
+    }
+    static class StreamUtils
+    {
+        public static byte[] ReadBytes(this MemoryStream s, int Len)
+        {
+            byte[] Ret = new byte[Len];
+            s.Read(Ret, 0, Len);
+            return Ret;
+        }
+    }
+    public enum ChannelOperation : byte
+    {
+        RemoteToLocal = 0,
+        LocalToRemote = 1,
     }
 }
