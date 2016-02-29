9d7f7eb508cba038e62453d248372319846df860
Threadnaught <patrickhingley@live.co.uk>
2016-02-29 22:04:38 +0000

    minor restructure

--- CDS/CDS.Communication/ConnectionPoolManager.cs
+++ CDS/CDS.Communication/ConnectionPoolManager.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CDS.Communication
 {
-    public static class ConnectionPoolManager
+    public static class MasterConnectionPool
     {
-        public static List<ConnectionPool> Pools = new List<ConnectionPool>();
+        //MANAGES ALL CONNECTIONS AND INTERFACES TO REST OF PROGRAM
+        public static List<Connection> Connections = new List<Connection>();
+        public static List<Client> Clients = new List<Client>();
+        public static void MessageReceivedFromConnection(Stream s, int Length, Connection con)
+        {
+
+        }
     }
 }
--- CDS/CDS.Communication/Message.cs
+++ CDS/CDS.Communication/Message.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace CDS.Communication
 {
-    public abstract class ConnectionPool
+    public class Message
     {
-
     }
 }
