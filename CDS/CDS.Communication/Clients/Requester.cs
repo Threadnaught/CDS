af713aac468271854bc800e1a0f4fc27333e6372
Threadnaught <patrickhingley@live.co.uk>
2016-03-12 22:07:32 +0000

    WHY

---
+++ CDS/CDS.Communication/Clients/Requester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS.Communication
+{
+    public class Requester
+    {
+        //makes requests to remote server, and processes responses from server
+        public Guid RequesterID; //this ID
+        public List<Request> OutstandingRequests = new List<Request>();
+        public void ReceiveResponse(Response r)
+        {
+            foreach (Request re in OutstandingRequests)
+            {
+                if (re.MessageID == r.MessageID)
+                {
+                    
+                }
+            }
+        }
+        public bool HasRequest(Guid MessageID)
+        {
+            return OutstandingRequests.FirstOrDefault(r => { return r.MessageID == MessageID; }) != null;
+        }
+    }
+}
---
+++ CDS/CDS.Communication/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CDS.Communication
+{
+    public static class Utils
+    {
+
+        public static byte[] ReadFromStream(this Stream s, int len)
+        {
+            //util to read len bytes from stream s
+            byte[] ret = new byte[len];
+            s.Read(ret, 0, len);
+            return ret;
+        }
+    }
+}
